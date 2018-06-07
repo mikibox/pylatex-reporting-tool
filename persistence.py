166172698c4fb5ef557240cc0df37564e004c238
mikibox <miquelfairmanfaiman@gmail.com>
2018-06-07 10:59:58 +0200

    creating initial tables and objects

--- persistence.py
+++ persistence.py
@@ -1,22 +1,42 @@
 import sqlite3
 
-sqlite_file = "database/my_db.sqlite"
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
+conn = None
 
-c.execute(
-    '''
-    CREATE TABLE IF NOT EXISTS evidence (
-    id integer PRIMARY KEY,
-    file text NOT NULL,
-    description text,
-    name text,
-    vulnerability text);
-     '''
-)
 
-# Committing changes and closing the connection to the database file
-conn.commit()
-conn.close()
+def create_connection(db_file):
+    try:
+        return sqlite3.connect(db_file)
+    except Exception as exc:
+        print(exc)
 
-conn.close()
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Exception as e:
+        raise e
+
+
+def main():
+    database = "database/my_db.sqlite"
+
+    sql_create_evidences_table = """
+        CREATE TABLE IF NOT EXISTS evidence (
+        id integer PRIMARY KEY,
+        file text NOT NULL,
+        description text,
+        name text,
+        vulnerability text); """
+    global conn
+    conn = create_connection(database)
+    if conn is None:
+        print("Error! cannot create the database connection.")
+        quit()
+    else:
+        create_table(conn, sql_create_evidences_table)
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
