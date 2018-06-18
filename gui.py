2702e5466bb0c9902d4d0c7b20a354d3f062f307
mikibox <miquelfairmanfaiman@gmail.com>
2018-06-18 10:38:19 +0200

    adding file selection wip

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from tkinter import Tk, Text, TOP, BOTH, X, Y, N, LEFT, messagebox, BOTTOM, Toplevel
+from tkinter import Tk, Text, TOP, BOTH, X, Y, N,S,W,E, LEFT, messagebox, BOTTOM, Toplevel, filedialog, StringVar
 from tkinter.ttk import Frame, Label, Entry, Combobox, Button
 import sqlalchemy_data as db
 from sqlalchemy_model import Evidence, Project
@@ -35,7 +35,7 @@ class ProjectSelector(Frame):
         self.project_cmbx = Combobox(frame1)
         self.project_cmbx['values'] = [x.name for x in db.get_all_projects()]
         self.project_cmbx.bind('<<ComboboxSelected>>', self.project_selected)
-        self.project_cmbx.pack(fill=X)
+        self.project_cmbx.pack(fill=X, padx=5, pady=5)
 
         self.frame_evidence.pack(fill=X)
 
@@ -67,6 +67,7 @@ class EvidenceWindow(Frame):
 
     def __init__(self, master):
         self.master = master
+        self.master.geometry("500x300+300+300")
         self.master.title("Evidence-Selector")
         self.frame = Frame(self.master)
         self.frame_footer = Frame(self.master)
@@ -75,19 +76,25 @@ class EvidenceWindow(Frame):
         super().__init__()
 
     def init_ui(self):
+        row=1
         first_column_width = 15
+        second_column_width = 60
 
-        lbl_name = Label(self.frame, text="Name", width=first_column_width)
-        lbl_name.pack(side=LEFT, padx=5, pady=5)
+        lbl_name = Label(self.frame, text="Name").grid(row=row, column=0)
+        self.evidence['name'] = Entry(self.frame, width=second_column_width).grid(row=row, column=1)
 
-        self.evidence['name'] = Entry(self.frame)
-        self.evidence['name'].pack(fill=X, padx=5, expand=True)
+        row+=1
+        lbl_description = Label(self.frame, text="Description", width=first_column_width).grid(row=row, column=0)
+        self.evidence['description'] = Entry(self.frame, width=second_column_width).grid(row=row, column=1)
 
-        lbl_description = Label(self.frame, text="Description", width=first_column_width)
-        lbl_description.pack(side=LEFT, padx=5, pady=5)
+        row += 1
+        lbl_filepath = Label(self.frame, text="Files", width=first_column_width).grid(row=row, column=0)
+        self.value_filepath = Label(self.frame, text="", width=first_column_width).grid(row=row, column=1)
+        bttn_create_evidence = Button(self.frame, text="Add File", command=self.add_file).grid(row=row, column=2)
 
-        self.evidence['description'] = Entry(self.frame)
-        self.evidence['description'].pack(fill=X, padx=5, expand=True)
+
+
+
 
         bttn_create_evidence = Button(self.frame_footer, text="Create", command=self.create_incidence)
         bttn_create_evidence.pack(side=BOTTOM, padx=5, pady=5)
@@ -102,45 +109,14 @@ class EvidenceWindow(Frame):
 
         print(project.evidences)
 
-
-class Example(Frame):
-
-    def __init__(self, message):
-        super().__init__()
-        self.init_ui()
-
-    def init_ui(self):
-        self.master.title("Review")
-        self.pack(fill=BOTH, expand=True)
-
-        first_column_width = 10
-
-        frame1 = Frame(self)
-        frame1.pack(fill=X)
-
-        lbl1 = Label(frame1, text="Title", width=first_column_width)
-        lbl1.pack(side=LEFT, padx=5, pady=5)
-
-        entry1 = Entry(frame1)
-        entry1.pack(fill=X, padx=5, expand=True)
-
-        frame2 = Frame(self)
-        frame2.pack(fill=X)
-
-        lbl2 = Label(frame2, text="Author", width=first_column_width)
-        lbl2.pack(side=LEFT, padx=5, pady=5)
-
-        entry2 = Entry(frame2)
-        entry2.pack(fill=X, padx=5, expand=True)
-
-        frame3 = Frame(self)
-        frame3.pack(fill=BOTH, expand=True)
-
-        lbl3 = Label(frame3, text="Review", width=first_column_width)
-        lbl3.pack(side=LEFT, anchor=N, padx=5, pady=5)
-
-        txt = Text(frame3)
-        txt.pack(fill=BOTH, pady=5, padx=5, expand=True)
+    def add_file(self):
+        file_path = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                   filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+        if file_path:
+            self.evidence['file_path'] = file_path
+            print(self.evidence)
+        else:
+            messagebox.showinfo('Info', 'No folder was selected')
 
 
 def main():
