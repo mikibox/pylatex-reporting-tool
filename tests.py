44ef9da7e2a4a091d2ec66af7c4a3244d2f98fc7
mikibox <miquelfairmanfaiman@gmail.com>
2018-07-19 14:45:36 +0200

    fixing tests, adding teardown

--- tests.py
+++ tests.py
@@ -1,13 +1,21 @@
 import unittest
 import os
+from sqlalchemy_model import create_database
+import sqlalchemy_data as db
 
 
 class DatabaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.db_path = 'database/my_db.sqlite'
+        self.db = 'db_tests'
+        self.db_path = 'database/{}.sqlite'.format(self.db)
+        create_database(self.db)
+        db.populate_database(self.db)
 
-    def test_default_widget_size(self):
+    def test_database_exists(self):
         self.assertEqual(os.path.exists(self.db_path), True, 'database file not found')
+
+    def tearDown(self):
+        os.remove(self.db_path)
 
 
 if __name__ == '__main__':
